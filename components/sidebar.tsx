c93d2f1fbe7ee2f45182327255b9106945148aa1
Dakshin Karthik Sridhar <66800136+dakshinkarthik@users.noreply.github.com>
2023-11-15 21:07:38 +0530

    Dashboard fully completed (with Shadcn-ui card component)

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,8 +5,9 @@ import Image from "next/image";
 import Link from "next/link"
 import { Montserrat } from "next/font/google";
 
-import {cn} from '@/lib/utils'
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { cn } from '@/lib/utils';
 
 const monsterrat = Montserrat({
     weight: "600", 
@@ -58,6 +59,7 @@ const routes = [
 ];
 
 const Sidebar= () => {
+    const pathname = usePathname();
     return (	
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -77,10 +79,8 @@ const Sidebar= () => {
                         <Link 
                         href={route.href}
                         key={route.href}
-                        className="text-sm group flex p-3 w-full 
-                        justify-start font-medium cursor-pointer
-                        hover:text-white hover:bg-white/10 rounded-lg
-                        transition"
+                        className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/20 rounded-lg transition",
+                        pathname === route.href ? "text-white bg-white/10" : "text-zinc-500")}
                         >
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
